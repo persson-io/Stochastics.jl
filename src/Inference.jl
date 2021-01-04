7594b8257286661f79a57af2684b33b7a92089fb
persson-io <rasmus@persson.io>
2021-01-04 16:57:52 +0100

    Added more reference variables

--- src/Inference.jl
+++ src/Inference.jl
@@ -3,18 +3,23 @@ include("DescriptiveStatistics.jl")
 
 """
 """
-function normal_reference__mu(μ::AbstractFloat, x::Vector, σ²=nothing)
+function normal_reference_mu_unknown_variance(μ::AbstractFloat, x::Vector)
     n = length(x) 
     x̅ = sample_mean(x)
-    if σ² === nothing
-        s = √(sample_variance(x))
-        Rμ = (x̅ - μ) / (s * √(n))
-        return Rμ
-    elseif σ² ≠ nothing
-        σ = √(σ²)
-        Rμ = (x̅ - μ) / (σ * √(n))
-        return Rμ
-    end 
+    s = √(sample_variance(x))
+    Rμ = (x̅ - μ) / (s * √(n))
+    return Rμ
+end
+
+
+"""
+"""
+function normal_reference_mu_known_variance(μ::AbstractFloat, σ²::AbstractFloat, x::Vector)
+    n = length(x) 
+    x̅ = sample_mean(x)
+    σ = √(σ²)
+    Rμ = (x̅ - μ) / (σ * √(n))
+    return Rμ 
 end
 
 
@@ -30,8 +35,40 @@ end
 
 """
 """
-function normal_reference_two_sample_mu()
-    return nothing    
+function normal_reference_two_sample_mu_known_variance(μᵪ::AbstractFloat, μᵧ::AbstractFloat, σ²ᵪ::AbstractFloat, σ²ᵧ::AbstractFloat, x::Vector, y::Vector)
+    nᵪ = length(x)
+    nᵧ = length(y)
+    x̅ = sample_mean(x) 
+    y̅ = sample_mean(x)
+    Rμᵪμᵧ = ((x̅ - y̅) - (μᵪ - μᵧ)) / √(σ²ᵪ / nᵪ + σ²ᵧ / nᵧ)
+    return Rμᵪμᵧ
+end
+
+
+"""
+"""
+function normal_reference_two_sample_mu_unknown_equal_variance(μᵪ::AbstractFloat, μᵧ::AbstractFloat, x::Vector, y::Vector)
+    nᵪ = length(x)
+    nᵧ = length(y)
+    x̅ = sample_mean(x) 
+    y̅ = sample_mean(x)
+    s = √(two_sample_variance(x, y))
+    Rμᵪμᵧ = ((x̅ - y̅) - (μᵪ - μᵧ)) / (s * √(1 / nᵪ + 1 / nᵧ))  
+    return Rμᵪμᵧ
+end
+
+
+"""
+"""
+function normal_reference_two_sample_mu_unknown_variance(μᵪ::AbstractFloat, μᵧ::AbstractFloat, x::Vector, y::Vector)
+    nᵪ = length(x)
+    nᵧ = length(y)
+    x̅ = sample_mean(x) 
+    y̅ = sample_mean(x)
+    s²ᵪ = sample_variance(x)
+    s²ᵧ = sample_variance(y)
+    Rμᵪμᵧ = ((x̅ - y̅) - (μᵪ - μᵧ)) / √(s²ᵪ / nᵪ + s²ᵧ / nᵧ)
+    return Rμᵪμᵧ   
 end
 
 
