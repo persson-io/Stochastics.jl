b6d832646b08c9e98ee2530f4a5301b7478ef3d1
persson-io <rasmus@persson.io>
2021-04-25 19:57:34 +0200

    Todo list

--- src/Stochastics.jl
+++ src/Stochastics.jl
@@ -12,3 +12,8 @@ end
 
 # TODO: ML-estimation of distributions.
 # TODO: Least-square-estimation of distributions.
+# TODO: Add gamma distribution.
+# TODO: Add t-distribution.
+# TODO: Add chi squared distribution.
+# TODO: Create functions for standard intervalls. 
+# TODO: Add some non parametric methods.
