f77f7c201f6de9cf70984f9d4666b871e7d54feb
persson-io <rasmus@persson.io>
2021-06-30 20:02:52 +0200

    Removed TODO

--- src/Stochastics.jl
+++ src/Stochastics.jl
@@ -10,11 +10,3 @@ include("Regression.jl")
 
 
 end
-
-# TODO: ML-estimation of distributions.
-# TODO: Least-square-estimation of distributions.
-# TODO: Add gamma distribution.
-# TODO: Add t-distribution.
-# TODO: Add chi squared distribution.
-# TODO: Create functions for standard intervalls. 
-# TODO: Add some non parametric methods.
